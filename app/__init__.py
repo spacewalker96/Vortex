61e175126eeedb1ce943e33c36397b67854e3681
Iliass Raihani <i.raihani@outlook.com>
2021-07-19 20:50:14 +0100

    test get_links

--- app/__init__.py
+++ app/__init__.py
@@ -6,22 +6,23 @@ from app.modules.vortex import Vortex
 
 def main():
     vortex_telecontact = Vortex(telecontact)
-    vortex_telecontact.get_results(3, 4)
+    vortex_telecontact.get_results(1, 5)
     print(vortex_telecontact.stats)
-    vortex_telecontact.extract_data()
-    print(vortex_telecontact.records)
+    # vortex_telecontact.extract_data()
+    # print(vortex_telecontact.records)
+    print(vortex_telecontact.links)
 
-    vortex_charika = Vortex(charika)
-    vortex_charika.get_results(1, 2)
-    print(vortex_charika.stats)
-    vortex_charika.extract_data()
-    print(vortex_charika.records)
+    # vortex_charika = Vortex(charika)
+    # vortex_charika.get_results(1, 2)
+    # print(vortex_charika.stats)
+    # vortex_charika.extract_data()
+    # print(vortex_charika.records)
 
-    db = Database(database.DATABASE)
-    db.store_multiple_dict(vortex_telecontact.records, telecontact.NAME)
-    db.store_multiple_dict(vortex_charika.records, charika.NAME)
-    db.fetch(""" SELECT * FROM companies """)
-    db.disconnect()
+    # db = Database(database.DATABASE)
+    # db.store_multiple_dict(vortex_telecontact.records, telecontact.NAME)
+    # db.store_multiple_dict(vortex_charika.records, charika.NAME)
+    # db.fetch(""" SELECT * FROM companies """)
+    # db.disconnect()
 
 
 if __name__ == "__main__":
